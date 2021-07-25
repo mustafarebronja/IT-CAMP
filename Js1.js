b9c9144b3e427a5199a22e06bfcf07da62bc367a
mustafarebronja <mustafa.rebronja3322@gmail.com>
2021-07-25 12:54:50 +0200

    Vezba

---
+++ Js1.js
@@ -0,0 +1,10 @@
+alert("yuegfyegfyge")
+
+const email =document.querySelector("#email");
+const password =document.querySelector("#password");
+const font =document.querySelector("#form");
+
+console. log("form");
+
+
+form.onsubmit =
